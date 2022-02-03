2812c60adfea6c26e8bd85d3ebc281461601968f
Your Name <you@example.com>
2022-02-03 15:23:40 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles.css';
+import AddThoughtForm from './AddThoughtForm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AddThoughtForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
